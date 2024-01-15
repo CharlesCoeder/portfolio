bf0d80a893b767ae11f3f6b19c117d82b5c2161d
CharlesCoeder <croederdev@gmail.com>
2024-01-15 15:50:15 -0500

    set background & text color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-slate-50 text-slate-950`}>
+        {children}
+      </body>
     </html>
   );
 }
