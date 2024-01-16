4dcfebfa4044f1f5aab34d3a45159e284762ec20
CharlesCoeder <croederdev@gmail.com>
2024-01-16 13:19:55 -0500

    Add intro paragraph

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -6,12 +6,12 @@ import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[60rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ type: "tween", duration: 0.2 }}
+          transition={{ type: "tween", duration: 0.3 }}
         >
           <Image
             src="/portrait.png"
@@ -24,6 +24,19 @@ export default function Intro() {
           />
         </motion.div>
       </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">{`Hi, I'm Charlie!`}</span>{" "}
+        {`I'm an aspiring `}
+        <span className="font-bold">software engineer</span> with experience
+        building <span className="font-bold">fullstack applications.</span> I
+        enjoy creating <span className="italic">websites and mobile apps</span>.
+        My focus is <span className="underline">React(Next.js)</span> and{" "}
+        <span className="underline">React Native</span>.
+      </motion.p>
     </section>
   );
 }
