4952b53cb63d8f94ad2b0a5be463f0a8e8abbec9
CharlesCoeder <croederdev@gmail.com>
2024-01-16 14:00:33 -0500

    Add buttons to intro component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center flex-col">
+    <main className="flex items-center flex-col px-4">
       <Intro />
     </main>
   );
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -37,6 +41,52 @@ export default function Intro() {
         My focus is <span className="underline">React(Next.js)</span> and{" "}
         <span className="underline">React Native</span>.
       </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-slate-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 hover:bg-slate-950 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full
+        outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer
+        border border-black/10"
+          href="/Charles_Roeder_Resume.pdf"
+          download
+        >
+          Download Resume{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-slate-700 hover:text-slate-900 flex items-center gap-2 rounded-full
+          focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer
+        border border-black/10"
+          href="https://www.linkedin.com/in/charlie-roeder/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-slate-700 hover:text-slate-900 flex items-center gap-2 rounded-full text-[1.35rem]
+          focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer
+        border border-black/10"
+          href="https://github.com/CharlesCoeder"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
