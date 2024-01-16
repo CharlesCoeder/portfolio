06d2a6ba662088fe129394761732aabd5495156e
CharlesCoeder <croederdev@gmail.com>
2024-01-16 14:10:59 -0500

    Section divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
+import Divider from "@/components/divider";
 
 export default function Home() {
   return (
     <main className="flex items-center flex-col px-4">
       <Intro />
+      <Divider />
     </main>
   );
 }
