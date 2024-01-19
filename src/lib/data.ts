19c907d3c02291047f99ddd3d9446737524374c8
CharlesCoeder <croederdev@gmail.com>
2024-01-19 12:28:21 -0500

    Projects section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,7 @@
+import agendamasterImg from "../../public/agenda-master.png";
+import sonasenseImg from "../../public/sona-sense.png";
+import subsageImg from "../../public/subsage.png";
+
 export const links = [
   {
     name: "Home",
@@ -22,5 +26,36 @@ export const links = [
   {
     name: "Contact",
     hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Agenda Master",
+    description:
+      "An AI-Assisted task management platform for prospective college students.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "ChatGPT API"],
+    imageUrl: agendamasterImg,
+  },
+  {
+    title: "SonaSense",
+    description:
+      "A music streaming platform geared with musical-theory based filtering features.",
+    tags: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Tailwind",
+      "Supabase",
+      "AWS API Gateway/Lambda functions",
+    ],
+    imageUrl: sonasenseImg,
+  },
+  {
+    title: "SubSage",
+    description:
+      "My passion project! A cross-platform app for language learners to seamlessly watch native content. Popup dictionaries allow for streamlined studying of subtitles.",
+    tags: ["React Native", "TypeScript", "WatermelonDB"],
+    imageUrl: subsageImg,
   },
 ] as const;
