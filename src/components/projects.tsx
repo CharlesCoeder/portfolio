447584d04de4758e3df9ed6983ebf1c072a5bbf7
CharlesCoeder <croederdev@gmail.com>
2024-01-25 12:38:18 -0500

    Change active section with scrolling

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { projectsData } from "@/lib/data";
 import SectionHeading from "./section-heading";
 import Project from "./project";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Projects() {
+  const { ref, inView } = useInView({ threshold: 0.5 });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Projects");
+    }
+  }, [inView]);
+
   return (
-    <section id="projects" className="mb-28 scroll-mt-28">
+    <section ref={ref} id="projects" className="mb-28 scroll-mt-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
