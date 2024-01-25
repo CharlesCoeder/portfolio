a531736a0d519ec0247ae3b1f72efa47fcf65b19
CharlesCoeder <croederdev@gmail.com>
2024-01-25 11:26:50 -0500

    Client project card component with transition

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { projectsData } from "@/lib/data";
 import SectionHeading from "./section-heading";
-import Image from "next/image";
+import Project from "./project";
 
 export default function Projects() {
   return (
-    <section>
+    <section className="mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
@@ -14,41 +14,6 @@ export default function Projects() {
           </React.Fragment>
         ))}
       </div>
-    </section>
-  );
-}
-
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return (
-    <section
-      className="group bg-slate-100 max-w-[42rem] border 
-    border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0
-    hover:bg-slate-200 transition cursor-pointer"
-    >
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex-col h-full">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-slate-700">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2">
-          {tags.map((tag, index) => (
-            <li
-              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-              key={index}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <Image
-        src={imageUrl}
-        alt="Project"
-        quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] transition"
-      />
     </section>
   );
 }
