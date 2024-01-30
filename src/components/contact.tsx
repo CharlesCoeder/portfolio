daff1bbb0dcf01d7107566d0360e3e7b5bfe3f49
CharlesCoeder <croederdev@gmail.com>
2024-01-30 18:13:33 -0500

    Add subtle fade-in transition to contact form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Contact() {
+  const ref = useSectionInView("Contact", 0.9);
+
   return (
-    <section
+    <motion.section
+      ref={ref}
       id="contact"
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 0.5,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
       <SectionHeading>Contact me</SectionHeading>
       <p className="text-slate-700 -mt-6">
@@ -40,6 +59,6 @@ export default function Contact() {
           <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
