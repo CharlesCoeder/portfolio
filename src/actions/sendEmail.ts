9ea3458bc414f030a86ebc294ee1eabe237df030
CharlesCoeder <croederdev@gmail.com>
2024-01-30 19:12:29 -0500

    Implement sendEmail

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -1,8 +1,35 @@
 "use server";
 
+import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
   "use server";
-  console.log("Running on server");
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  if (!validateString(senderEmail, 500)) {
+    return { error: "Invalid sender email" };
+  }
+
+  if (!validateString(message, 5000)) {
+    return { error: "Invalid message" };
+  }
+
+  try {
+    await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "croederdev@gmail.com",
+      subject: "Message from contact form",
+      text: message as string,
+      reply_to: senderEmail as string,
+    });
+
+    console.log("email sent");
+  } catch (error: unknown) {
+    return { error: getErrorMessage(error) };
+  }
 };
